be2ffed3a04ced297d76564159c46cae603f50fa
tamil-selvan-13 <selvan1392005@gmail.com>
2026-04-15 18:57:51 +0530

    feat: initial commit with Next.js boilerplate and design system

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="w-full py-10 bg-black text-white">Footer</footer>;
+}
---
+++ components/layout/Navigation.tsx
@@ -0,0 +1,3 @@
+export default function Navigation() {
+  return <nav className="fixed top-0 w-full z-50">Navigation</nav>;
+}
